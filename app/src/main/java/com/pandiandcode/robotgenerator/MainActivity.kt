9e631e600166bd4b9ffa73812e127d630d20277e
Rocio Ortega <rocioortegapriego@gmail.com>
2018-05-06 17:29:16 +0200

    Get view elements we added to the layout

--- app/src/main/java/com/pandiandcode/robotgenerator/MainActivity.kt
+++ app/src/main/java/com/pandiandcode/robotgenerator/MainActivity.kt
@@ -2,11 +2,26 @@ package com.pandiandcode.robotgenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var image: ImageView
+    lateinit var input: EditText
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        image = findViewById(R.id.image)
+        input = findViewById(R.id.input)
+        button = findViewById(R.id.button)
     }
 }
